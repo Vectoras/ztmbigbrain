526d8bc2fa141fa8727b556613bebe6ec6a39c9f
Ionut Boghean <ionut.boghean.conturi@gmail.com>
2021-05-31 13:23:37 +0200

    Working app

---
+++ js/npm_modules_imports.js
@@ -0,0 +1,1 @@
+let Clarifai = require("clarifai");
--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
-import VanillaTilt from "../node_modules/vanilla-tilt/src/vanilla-tilt.js";
 import * as particles_module from "../node_modules/particlesjs/src/particles.js";
-import Clarifai from "clarifai";
+import VanillaTilt from "../node_modules/vanilla-tilt/src/vanilla-tilt.js";
 
 // ParticlesJS
 window.onload = function () {
@@ -43,25 +42,55 @@ window.onload = function () {
   });
 };
 
+// Clarifai
+const app = new Clarifai.App({
+  apiKey: "833cf82f5bb24fb99dcd15f6c1c1448d",
+});
+
+// global variables
+let imageEl;
+
 // caching DOM elements
 const imageSourceEl = document.querySelector("#image-source");
 const formEl = document.querySelector("#app-form");
-const imageEl = document.querySelector("#current-image");
-
-// global variables
-let currentImage = "";
-const API_KEY = "";
+const imageContainerEl = document.querySelector("#selected-image-container");
 
 // source change
 imageSourceEl.addEventListener("change", (e) => {
-  currentImage = e.currentTarget.value;
-  console.log(currentImage);
-  imageEl.src = currentImage;
+  // reset the container
+  imageContainerEl.textContent = "";
+  // create image element
+  imageEl = document.createElement("img");
+  imageEl.src = e.currentTarget.value;
   imageEl.title = "Selected Image";
+  // append
+  imageContainerEl.appendChild(imageEl);
 });
 
 // form submission (request to detect)
 formEl.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log("Need to implement request to API");
+  app.models
+    .predict("a403429f2ddf4b49b307e318f00e528b", imageEl.src)
+    .then((response) => {
+      let faceBoxesArray = response.outputs[0].data.regions.map((currentRegion) => {
+        return currentRegion.region_info.bounding_box;
+      });
+      // draw the boxes
+      faceBoxesArray.forEach((faceBox) => {
+        // create element and add css build class
+        const faceBoxEl = document.createElement("div");
+        faceBoxEl.classList.add("face-box");
+        // define points for absolute positioning
+        faceBoxEl.style.top = `${faceBox.top_row * 100}%`;
+        faceBoxEl.style.right = `${(1 - faceBox.right_col) * 100}%`;
+        faceBoxEl.style.bottom = `${(1 - faceBox.bottom_row) * 100}%`;
+        faceBoxEl.style.left = `${faceBox.left_col * 100}%`;
+        // append
+        imageContainerEl.appendChild(faceBoxEl);
+      });
+    })
+    .catch((err) => {
+      console.log("An error happened: ", err);
+    });
 });
