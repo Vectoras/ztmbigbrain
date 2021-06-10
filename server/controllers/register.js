d86927ad089366fbdcd8cd2918166a3ee87cff62
Ionut Boghean <ionut.boghean.conturi@gmail.com>
2021-06-10 16:26:09 +0200

    Found not commited things after a couple of days - did not check if everything works

--- server/controllers/register.js
+++ server/controllers/register.js
@@ -1,6 +1,6 @@
 const handleRegister = (bcrypt, db) => (req, res) => {
   // checks
-  if (!req.body.email || !req.body.password || !req.body.name) {
+  if (!req.body.email || !req.body.password || !req.body) {
     return res.status(400).json("Invalid form submission!");
   }
 
